3ca07f7cb74c7f6c052d27da5b97d779f1214d46
dream_pep <ab090929@163.com>
2026-04-25 16:36:37 +0800

    Allow dev channel to create releases

--- script/version.ts
+++ script/version.ts
@@ -19,7 +19,7 @@ if (!Script.preview) {
   const release = await $`gh release view v${Script.version} --json tagName,databaseId`.json()
   output.push(`release=${release.databaseId}`)
   output.push(`tag=${release.tagName}`)
-} else if (Script.channel === "beta") {
+} else if (Script.channel === "beta" || Script.channel === "dev") {
   await $`gh release create v${Script.version} -d --title "v${Script.version}" --repo ${process.env.GH_REPO}`
   const release =
     await $`gh release view v${Script.version} --json tagName,databaseId --repo ${process.env.GH_REPO}`.json()
